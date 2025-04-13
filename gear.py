753f5b8b15c467f3db7cd1dcb38aa141e7f68a5d
MoritzKo <64729870+MoritzKo@users.noreply.github.com>
2025-04-13 11:41:31 +0200

    Update gear.py

--- gear.py
+++ gear.py
@@ -1,2 +1,16 @@
-# THis will be a test
-# 
+# To calculate speed ~ cadence 
+
+import numpy as np
+
+durchmesser = 28*2.54
+
+Umfang = durchmesser * np.pi
+
+def geschwindigkeit(n_ritzel, n_kettenblatt):
+  ratio = n_kettenblatt/n_ritzel
+  # zurückgelegte Strecke pro Kurbelumdrehung
+  strecke = ratio * Umfang
+  cadence = np.arrange(50,151) 
+  # zurückgelegte Strecke pro Minute
+  strecke_pro_min = cadence * strecke
+
