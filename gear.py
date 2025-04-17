de5ad1f27fb61e77606e53559fb9d560f01b111f
MoritzKo <64729870+MoritzKo@users.noreply.github.com>
2025-04-17 10:47:03 +0200

    Update gear.py

--- gear.py
+++ gear.py
@@ -2,16 +2,24 @@
 
 import numpy as np
 
-durchmesser = 28*2.54
+durchmesser = 28 * 2.54 / 100
 
 Umfang = durchmesser * np.pi
 
-def geschwindigkeit(n_ritzel, n_kettenblatt):
+def geschwindigkeit_bei_kadenz(n_ritzel, n_kettenblatt):
   ratio = n_kettenblatt/n_ritzel
   # zurückgelegte Strecke pro Kurbelumdrehung
   strecke = ratio * Umfang
-  cadence = np.arrange(50,151) 
+  # Kadenz als Umdrehungen pro Minute
+  cadence = np.arange(50,151) 
   # zurückgelegte Strecke pro Minute
-  strecke_pro_min = cadence * strecke
+  strecke_pro_stunde = cadence * strecke
+  # Umrechnung in km/h
+  km_pro_stunde = strecke_pro_stunde * 60 / 1000
+  return km_pro_stunde
+
+geschwindigkeit_bei_kadenz(15, 49)
+
+  
   
 
